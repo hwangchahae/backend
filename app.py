c031966ffc0a278820b1760cdedf55e4a1a2ba99
hwangchahae <wndal12@gmail.com>
2025-06-11 15:25:34 +0900

    commit

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask,render_template
 
 # 객체생성
 app = Flask(__name__)
@@ -6,6 +6,17 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     return render_template('index.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/projects')
+def projects():
+    return render_template('projects.html')
+
+
+
 
 if __name__ == '__main__':
     app.run(debug=True)
